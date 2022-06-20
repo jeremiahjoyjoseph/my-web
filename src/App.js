6cf00ec4196608f4fee8c4d478c223bb22e04d6e
Jeremiah Joy Joseph <jeremiahjoyjoseph97@gmail.com>
2022-06-21 03:22:45 +0530

    inProgress: theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import WebFont from 'webfontloader';
+import { useTheme } from './theme/useTheme';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './theme/GlobalStyles';
 
 function App() {
+  const { theme, themeLoaded, getFonts } = useTheme();
+  const [selectedTheme, setSelectedTheme] = useState(theme);
+
+  useEffect(() => {
+    setSelectedTheme(theme);
+  }, [themeLoaded]);
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: getFonts(),
+      },
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {themeLoaded && (
+        <ThemeProvider theme={selectedTheme}>
+          <GlobalStyles />
+          <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className='App-link'
+              href='https://reactjs.org'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Learn React
+            </a>
+          </header>
+        </ThemeProvider>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as themes from './theme/schema.json';
+import { setToLS } from './utils/storage';
+
+const Index = () => {
+  setToLS('all-themes', themes.default);
+  return <App />;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>
 );
 
