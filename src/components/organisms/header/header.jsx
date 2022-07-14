9ab2fa20500ece40303e6e5e32aa35dd5f6461ee
Jeremiah Joy Joseph <jeremiahjoyjoseph97@gmail.com>
2022-07-14 23:21:36 +0530

    fix: header

--- src/components/organisms/header/header.jsx
+++ src/components/organisms/header/header.jsx
@@ -8,33 +8,22 @@ const Header = (props) => {
   const navigate = useNavigate();
   const location = useLocation();
   const [canGoBack] = useState(location.key !== 'default' ? true : false);
-  console.log(location.key);
 
   const handleBack = () => {
-    navigate(-1);
+    if (canGoBack) {
+      navigate(-1);
+    } else {
+      navigate('/');
+    }
   };
-  const handleHome = () => {
-    navigate('/');
-  };
+
   return (
     <Wrapper>
-      {canGoBack && (
-        <Fragment>
-          <IconButton
-            name='arrow_left'
-            style={{ paddingLeft: 0 }}
-            onClick={handleBack}
-          />
-          <h3 class='bold'>/</h3>
-        </Fragment>
-      )}
-      <h2
-        class='bold'
-        style={{ marginLeft: 10, cursor: 'pointer' }}
-        onClick={handleHome}
-      >
-        Home
-      </h2>
+      <IconButton
+        name='arrow_left'
+        style={{ paddingLeft: 0 }}
+        onClick={handleBack}
+      />
     </Wrapper>
   );
 };
