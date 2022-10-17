fe041c60e2507d91335c0c10febf7d48cf61021f
Jeremiah Joy Joseph <jeremiahjoyjoseph97@gmail.com>
2022-10-17 23:18:05 +0530

    complete: cv change

--- src/data/cv.js
+++ src/data/cv.js
@@ -1,25 +1,25 @@
-import resume_pdf from '../assets/files/JeremiahJoyJoseph_Resume.PDF';
+import resume_pdf from "../assets/files/JeremiahJoyJosephResume.pdf";
 export const cv_data = {
-  title: 'CV',
+  title: "CV",
   dark_mode_switch: true,
   go_back_available: true,
   subtitle: {
-    type: 'text',
+    type: "text",
     data: [],
   },
   cards: [
     {
-      title: 'View',
-      subtitle: 'an online resume',
-      clickToAction: 'external',
-      link: 'https://zety.com/profile/jeremiahjoyjoseph',
+      title: "View",
+      subtitle: "an online resume",
+      clickToAction: "external",
+      link: "https://zety.com/profile/jeremiahjoyjoseph",
     },
     {
-      title: 'Download',
-      subtitle: 'as pdf',
-      clickToAction: 'download',
+      title: "Download",
+      subtitle: "as pdf",
+      clickToAction: "download",
       file: resume_pdf,
-      fileName: 'JeremiahJoyJoseph_Resume.pdf',
+      fileName: "JeremiahJoyJoseph_Resume.pdf",
     },
   ],
 };
